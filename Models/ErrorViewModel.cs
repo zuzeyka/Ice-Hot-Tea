7c78b56667eac13e3f8de590d25f5d2fdc8011c1
zuzeyka <stepan.tarasenko.26@gmail.com>
2024-01-25 21:01:22 +0200

    Init project files

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Ice_Hot_Tea.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
